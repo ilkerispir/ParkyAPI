54b342e23775c7b9762a5894ec3ed37b14a33f6c
ilkerispir <ikerispir@gmail.com>
2020-02-02 03:33:13 +0300

    ParkyAPI

---
+++ ParkyWeb/Controllers/HomeController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ParkyWeb.Models;
+using ParkyWeb.Models.ViewModel;
+using ParkyWeb.Repository.IRepository;
+
+namespace ParkyWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly INationalParkRepository _npRepo;
+        private readonly IAccountRepository _accRepo;
+        private readonly ITrailRepository _trailRepo;
+        public HomeController(ILogger<HomeController> logger, INationalParkRepository npRepo,
+            ITrailRepository trailRepo, IAccountRepository accRepo)
+        {
+            _npRepo = npRepo;
+            _trailRepo = trailRepo;
+            _logger = logger;
+            _accRepo = accRepo;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            IndexVM listOfParksAndTrails = new IndexVM()
+            {
+                NationalParkList = await _npRepo.GetAllAsync(SD.NationalParkAPIPath,HttpContext.Session.GetString("JWToken")),
+                TrailList = await _trailRepo.GetAllAsync(SD.TrailAPIPath, HttpContext.Session.GetString("JWToken")),
+            };
+            return View(listOfParksAndTrails);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            User obj = new User();
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async  Task<IActionResult> Login(User obj)
+        {
+            User objUser = await _accRepo.LoginAsync(SD.AccountAPIPath + "authenticate/", obj);
+            if (objUser.Token == null)
+            {
+                return View();
+            }
+
+            HttpContext.Session.SetString("JWToken", objUser.Token);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(User obj)
+        {
+            bool result = await _accRepo.RegisterAsync(SD.AccountAPIPath + "register/", obj);
+            if (result == false)
+            {
+                return View();
+            }
+
+            return RedirectToAction("Login");
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.Session.SetString("JWToken", "");
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# RESTful API with ASP .NET Core Web API - Create and Consume
+
+## Build RESTful API and learn how to consume them in ASP .NET Core the right way in a real world application.
+
+![intro](ScreenShot.png)
